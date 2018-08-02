63659d9d8bc026dc192f6f5ac14063efaaed5e99
steve-sienk <steve@sienkowski.com>
2018-08-01 22:29:29 -0600

    [#159485591] Initialize with generated project from https://start.spring.io/

---
+++ spring/src/main/kotlin/com/sienklogic/swarmsomethingsomething/SwarmSomethingSomethingApplication.kt
@@ -0,0 +1,11 @@
+package com.sienklogic.swarmsomethingsomething
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SwarmSomethingSomethingApplication
+
+fun main(args: Array<String>) {
+    runApplication<SwarmSomethingSomethingApplication>(*args)
+}
---
+++ spring/src/test/kotlin/com/sienklogic/swarmsomethingsomething/SwarmSomethingSomethingApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.sienklogic.swarmsomethingsomething
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class SwarmSomethingSomethingApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
